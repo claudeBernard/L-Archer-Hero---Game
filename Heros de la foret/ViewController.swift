7e65d6c0d007c39d31479537498f08108d7b2659
Claude-Bernard Millen <claude.bernard.m@hotmail.com>
2017-12-06 20:07:33 -0500

    Initial Commit

---
+++ Heros de la foret/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Heros de la foret
+//
+//  Created by Claude-Bernard Millen on 17-12-06.
+//  Copyright © 2017 Claude-Bernard Millen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
