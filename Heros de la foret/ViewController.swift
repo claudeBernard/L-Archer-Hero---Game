2a9fc4e77617903536bd2f3379422854a1214ad6
Claude-Bernard Millen <claude.bernard.m@hotmail.com>
2017-12-09 01:13:10 -0500

    first_version

--- Heros de la foret/ViewController.swift
+++ Heros de la foret/ViewController.swift
@@ -1,25 +1,189 @@
-//
-//  ViewController.swift
-//  Heros de la foret
-//
-//  Created by Claude-Bernard Millen on 17-12-06.
-//  Copyright © 2017 Claude-Bernard Millen. All rights reserved.
-//
-
+//===
 import UIKit
-
+//===
 class ViewController: UIViewController {
-
+    //---
+    @IBOutlet weak var archer: UIImageView!
+    @IBOutlet weak var showChanges: UILabel!
+    @IBOutlet weak var showScore: UILabel!
+    @IBOutlet weak var configUser: UILabel!
+    @IBOutlet weak var configScoreUser: UILabel!
+    @IBOutlet weak var configScoreUserMemorie: UILabel!
+    @IBOutlet weak var configContinue: UIButton!
+    @IBOutlet weak var placarConfig: UIView!
+    @IBOutlet weak var buttonConfig: UIButton!
+    @IBOutlet weak var buttonLaunch: UIButton!
+    @IBOutlet weak var slider: UISlider!
+    var archerDates: Float!
+    var arrow = UIImageView()
+    var money = UIImageView()
+    var aTimer: Timer!
+    var aTimerAnimationMoney: Timer!
+    var aTimerAnimationConfig: Timer!
+    var cos: Double!
+    var sin: Double!
+    var controlPlay: Bool!
+    var controlChanges: Int!
+    var scoreUser: Int!
+    var scoreMemory = 0
+    var receivedString = ""
+    var inputUser: String!
+    var usersMemory = UserDefaultsManager()
+    var tableUsers = [String: Int]()
+    var tableUsersFinal = [String: Int]()
+    var showPlacarConfig = false
+    //---
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        controlChanges = 5
+        showChanges.text = String(controlChanges)
+        scoreUser = 0
+        showScore.text = String(scoreUser)
+        money.frame = CGRect(x: Int((arc4random() % 200) + 300), y: Int((arc4random() % 170) + 30), width: 40, height: 60)
+        createMoney()
+        arrow.frame = CGRect(x: 0, y: 0, width: 40, height: 5)
+        archerDates = 0
+        createArrow()
+        controlPlay = true
+        inputUser = receivedString
+        memoryVerification()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //---
+    func memoryVerification() {
+        tableUsers = usersMemory.getValue(theKey: "heros") as! [String: Int]
+        let index = tableUsers.index(forKey: inputUser)
+        scoreMemory = tableUsers.values[index!]
     }
-
-
+    //---
+    @IBAction func movingArcher(_ sender: UISlider) {
+        archerDates = sender.value
+        UIView.animate(withDuration: 1.0, animations: {
+            self.archer.transform = CGAffineTransform(rotationAngle: CGFloat(-(sender.value)))
+        })
+    }
+    //---
+    func createArrow() {
+        cos = __cospi(Double(0))
+        sin = __sinpi(Double(0))
+        arrow.image = UIImage(named: "arrow.png")
+        arrow.layer.cornerRadius = 12.5
+        arrow.center.x = archer.center.x + CGFloat(40 - Int(archerDates*25))
+        arrow.center.y = archer.center.y - CGFloat(28 + Int(archerDates*25))
+        self.arrow.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+    }
+    //---
+    func createMoney() {
+        money.image = UIImage(named: "money.png")
+        money.layer.cornerRadius = 24
+        money.center.x = CGFloat(Int((arc4random() % 200) + 300))
+        money.center.y = CGFloat(Int((arc4random() % 170) + 30))
+        self.view.addSubview(money)
+    }
+    //---
+    func launchAnimation() {
+        cos = __cospi(Double(-(archerDates/3)))
+        sin = __sinpi(Double(-(archerDates/3)))
+        self.arrow.transform = CGAffineTransform(rotationAngle: CGFloat(-(archerDates)))
+        self.view.addSubview(arrow)
+        aTimer = Timer.scheduledTimer(timeInterval: 0.002, target: self, selector: #selector(animate), userInfo: nil, repeats: true)
+    }
+    //--
+    @objc func animate() {
+        controlPlay = false
+        if arrow.center.y < -30 || arrow.center.x > 735 || arrow.frame.intersects(money.frame){
+            if arrow.frame.intersects(money.frame) {
+                arrow.isHidden = true
+                aTimerAnimationMoney = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(animateMoney), userInfo: nil, repeats: true)
+                scoreUser = scoreUser + 10
+                if scoreUser > scoreMemory {
+                    let index = tableUsers.index(forKey: inputUser)
+                    tableUsers.values[index!] = scoreUser
+                    usersMemory.setKey(theValue: tableUsers as AnyObject, key: "heros")
+                }
+                showScore.text = String(scoreUser)
+            } else if arrow.center.y < -30 || arrow.center.x > 735 {
+                controlChanges = controlChanges - 1
+                showChanges.text = String(controlChanges)
+            }
+            aTimer.invalidate()
+            aTimer = nil
+            controlPlay = true
+            archer.image = UIImage(named: "hero1.png")
+            if controlChanges == 0 {
+                configContinue.isEnabled = false
+                showConfig ()
+            }
+        }
+        arrow.center.x += CGFloat(cos)
+        arrow.center.y += CGFloat(sin)
+    }
+    //---
+    @objc func animateMoney() {
+        money.center.y -= CGFloat(-6)
+        if money.center.y > 500 {
+            aTimerAnimationMoney.invalidate()
+            aTimerAnimationMoney = nil
+            createMoney()
+        }
+    }
+    //---
+    @IBAction func launchArrow(_ sender: UIButton) {
+        if controlPlay == true && controlChanges > 0{
+            archer.image = UIImage(named: "hero2.png")
+            arrow.isHidden = false
+            createArrow()
+            launchAnimation()
+        } else if controlPlay == true && controlChanges == 0 {
+            showConfig ()
+        }
+    }
+    //---
+    func showConfig () {
+        money.isHidden = true
+        configScoreUser.text = String(scoreUser)
+        configScoreUserMemorie.text = String(scoreMemory)
+        configUser.text = inputUser
+        buttonConfig.isEnabled = false
+        buttonLaunch.isEnabled = false
+        slider.isEnabled = false
+        aTimerAnimationConfig = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(animateConfig), userInfo: nil, repeats: true)
+    }
+    //---
+    @objc func animateConfig() {
+        if showPlacarConfig == false {
+            placarConfig.center.y -= CGFloat(-3)
+            if placarConfig.center.y > 120 {
+                aTimerAnimationConfig.invalidate()
+                aTimerAnimationConfig = nil
+                showPlacarConfig = true
+            }
+        } else {
+            placarConfig.center.y += CGFloat(-3)
+            if placarConfig.center.y < -120 {
+                aTimerAnimationConfig.invalidate()
+                aTimerAnimationConfig = nil
+                showPlacarConfig = false
+                money.isHidden = false
+                buttonConfig.isEnabled = true
+                buttonLaunch.isEnabled = true
+                slider.isEnabled = true
+            }
+        }
+    }
+    //---
+    @IBAction func config(_ sender: UIButton) {
+        showConfig ()
+    }
+    //---
+    @IBAction func reset(_ sender: UIButton) {
+        scoreUser = 0
+        showScore.text = String(scoreUser)
+        controlChanges = 5
+        showChanges.text = String(controlChanges)
+        showConfig ()
+        configContinue.isEnabled = true
+        memoryVerification()
+    }
+    //--
 }
 
---
+++ Heros de la foret/ViewControllerStart.swift
@@ -0,0 +1,74 @@
+import UIKit
+//===
+class ViewControllerStart: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+    //---
+    @IBOutlet weak var inputUser: UITextField!
+    @IBOutlet weak var tableRecords: UITableView!
+    var usersMemory = UserDefaultsManager()
+    var tableUsers = [String: Int]()
+    var inputLabelUser: String!
+    //---
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableRecords.backgroundColor = UIColor.clear
+        inputUser.layer.cornerRadius = 25
+        memorieVerification()
+        self.inputUser.delegate = self
+    }
+    //---
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let secondView = segue.destination as! ViewController
+        secondView.receivedString = inputUser.text!
+    }
+    //---
+    func memorieVerification() {
+        if usersMemory.doesKeyExist(theKey: "heros") {
+            tableUsers = usersMemory.getValue(theKey: "heros") as! [String: Int]
+        }
+    }
+    //---
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableUsers.count
+    }
+    //---
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: nil)
+        cell.backgroundColor = UIColor.clear
+        cell.textLabel?.textColor = UIColor(red: 75/255, green: 108/255, blue: 144/255, alpha: 1)
+        cell.textLabel?.font = UIFont(name: "American Typewriter", size: 20)
+        cell.textLabel?.text = "\([String](tableUsers.keys)[indexPath.row]) = \([Int](tableUsers.values)[indexPath.row]) "
+        return cell
+    }
+    //---
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    //---
+    @IBAction func IdentUser(_ sender: UIButton) {
+        var existUserInMemory = false
+        inputLabelUser = inputUser.text!
+        
+        if inputLabelUser != "" {
+            
+            if usersMemory.doesKeyExist(theKey: "heros") {
+                tableUsers = usersMemory.getValue(theKey: "heros") as! [String: Int]
+                for index in 0..<tableUsers.count {
+                    if  [String](tableUsers.keys)[index] == inputLabelUser {
+                        existUserInMemory = true
+                    }
+                }
+                if existUserInMemory == false {
+                    tableUsers[inputLabelUser] = 0
+                    usersMemory.setKey(theValue: tableUsers as AnyObject, key: "heros")
+                }
+                
+            } else {
+                tableUsers = [inputLabelUser: 0]
+                usersMemory.setKey(theValue: tableUsers as AnyObject, key: "heros")
+            }
+            
+        }
+    }
+    //---
+}
